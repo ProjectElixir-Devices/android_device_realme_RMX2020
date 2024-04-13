1d581745104af83b4e0f4825f3538b83cd6b81dd
TogoFire <togofire@mailfence.com>
2024-04-13 18:29:38 +0530

    RMX2020: Add support for ViPER4Android FX Redesign and build it
    Viper's Audio is software for various platforms, such as Android and PC, Cat.
    It is used to improve the sound in general, with the help of various sound effects,
    i.e. it is actually a system equalizer.
    
    Reference:
    https://github.com/TogoFire/packages_apps_ViPER4AndroidFX
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I4cdc7d278d01d91bb68048ea71850d049336af2b

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 echo 'Starting to clone stuffs needed for RMX2020'
-echo 'Cloning Kernel tree [1/6]'
+echo 'Cloning Kernel tree [1/7]'
 # Kernel
 rm -rf kernel/realme/RMX2020
 if [ ! -d "kernel/realme/RMX2020" ]; then
@@ -7,7 +7,7 @@ if [ ! -d "kernel/realme/RMX2020" ]; then
     git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_realme_RMX2020 -b UNO-RUI2-OSS kernel/realme/RMX2020
 fi
 
-echo 'Cloning Vendor blobs [2/6]'
+echo 'Cloning Vendor blobs [2/7]'
 # Vendor
 rm -rf vendor/realme/RMX2020
 if [ ! -d "vendor/realme/RMX2020" ]; then
@@ -15,7 +15,7 @@ if [ ! -d "vendor/realme/RMX2020" ]; then
     git clone https://github.com/ProjectElixir-Devices/vendor_realme_RMX2020 -b UNO vendor/realme/RMX2020
 fi
 
-echo 'Cloning Vendor-ims blobs [3/6]'
+echo 'Cloning Vendor-ims blobs [3/7]'
 # Vendor-ims
 rm -rf vendor/realme/RMX2020-ims
 if [ ! -d "vendor/realme/RMX2020-ims" ]; then
@@ -23,7 +23,7 @@ if [ ! -d "vendor/realme/RMX2020-ims" ]; then
     git clone https://github.com/ProjectElixir-Devices/vendor_realme_RMX2020-ims -b UNO vendor/realme/RMX2020-ims
 fi
 
-echo 'Cloning Mediatek Hardware [4/6]'
+echo 'Cloning Mediatek Hardware [4/7]'
 # Hardware
 rm -rf hardware/mediatek
 if [ ! -d "hardware/mediatek" ]; then
@@ -31,7 +31,7 @@ if [ ! -d "hardware/mediatek" ]; then
     git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-21 hardware/mediatek
 fi
 
-echo 'Cloning Mediatek SEpolicy [5/6]'
+echo 'Cloning Mediatek SEpolicy [5/7]'
 # SEpolicy
 rm -rf device/mediatek/sepolicy_vndr
 if [ ! -d "device/mediatek/sepolicy_vndr" ]; then
@@ -39,12 +39,20 @@ if [ ! -d "device/mediatek/sepolicy_vndr" ]; then
     git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-21 device/mediatek/sepolicy_vndr
 fi
 
-echo 'Cloning AOSP Clang 18.0.0 [6/6]'
+echo 'Cloning AOSP Clang 18.0.0 [6/7]'
 # Clang
 rm -rf prebuilts/clang/host/linux-x86/clang-r510928
 if [ ! -d "prebuilts/clang/host/linux-x86/clang-r510928" ]; then
     mkdir -p prebuilts/clang/host/linux-x86/clang-r510928
     git clone https://gitlab.com/projectelixiros/android_prebuilts_clang_host_linux-x86_clang-r510928 prebuilts/clang/host/linux-x86/clang-r510928
+fi
+
+echo 'Cloning ViPER4AndroidFX [7/7]'
+# ViPER4AndroidFX
+rm -rf packages/apps/ViPER4AndroidFX
+if [ ! -d "packages/apps/ViPER4AndroidFX" ]; then
+    mkdir -p packages/apps/ViPER4AndroidFX
+    git clone https://github.com/Koushikdey2003/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
 fi
 
 echo 'Completed, Now proceeding to lunch'
